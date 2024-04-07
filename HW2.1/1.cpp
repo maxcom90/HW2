d9f0d9af488be16b1ed913dd1ff06c50f481c61b
XXX <maxcom90@mail.ru>
2024-04-07 09:01:29 +0300

    Add HW1 and HW2

---
+++ HW2.1/1.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+
+class Calculator
+{
+public:
+
+    double num1;
+    double num2;
+    
+
+    double add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    double multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    double subtract_1_2(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    double subtract_2_1(double num1, double num2)
+    {
+        return num2 - num1;
+    }
+
+    double divide_1_2(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+    double divide_2_1(double num1, double num2)
+    {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1)
+    {
+        this->num1 = num1;
+        if (num1 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    bool set_num2(double num2)
+    {
+        this->num2 = num2;
+        if (num2 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
+
+
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    std::system("chcp 1251");
+    double numb1, numb2;
+    
+
+    
+
+    Calculator calculator;
+    
+
+    
+
+    for (;;)
+    {
+        std::cout << "Введите num1: ";
+        std::cin >> numb1;
+        calculator.num1 = numb1;
+
+        if (calculator.set_num1(numb1))
+        {
+           do
+           {
+               std::cout << "Введите num2: ";
+               std::cin >> numb2;
+               calculator.num2 = numb2;
+
+               if (calculator.set_num2(numb2))
+               {
+                   std::cout << "num1 + num2 = " << calculator.add(numb1, numb2) << std::endl;
+                   std::cout << "num1 - num2 = " << calculator.subtract_1_2(numb1, numb2) << std::endl;
+                   std::cout << "num2 - num1 = " << calculator.subtract_2_1(numb1, numb2) << std::endl;
+                   std::cout << "num1 * num2 = " << calculator.multiply(numb1, numb2) << std::endl;
+                   std::cout << "num1 / num2 = " << calculator.divide_1_2(numb1, numb2) << std::endl;
+                   std::cout << "num2 / num1 = " << calculator.divide_2_1(numb1, numb2) << std::endl;
+               }
+               else
+               {
+                   std::cout << "Неверный ввод!" << std::endl;
+               }
+           } while (numb2 == 0);
+           
+            
+        }
+        else
+        {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+    } 
+
+    
+  
+}
+
+
+
+   
+
+
---
+++ HW2.2/HW2.2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+std::string** creatarr(int numb_address, int numb_elements)
+{
+    std::string** arr = new std::string * [numb_address];
+    for (int i = 0; i < numb_address; i++)
+    {
+        arr[i] = new std::string[numb_elements];
+    }
+    return arr;
+}
+int main(int argc, char** argv)
+std::string a;
+std::ifstream fin("in.txt");
+std::ofstream fout("out.txt");
+if (fin.is_open())
+{    
+    fin >> a;
+    int numb_address = std::stoi(a);
+    int numb_elements = 4;
+    creatarr(numb_address, numb_elements);
+    std::string** arr1 = creatarr(numb_address, numb_elements);
+    for (int i = 0; i < numb_address; i++)
+    {
+        for (int j = numb_elements - 1; j >= 0; j--)
+        {
+            fin >> arr1[i][j];
+        }
+    }
+    std::cout << numb_address << std::endl;
+    for (int i = numb_address - 1; 0 <= i; i--)
+    {
+        for (int j = numb_elements - 1; 0 <= j; j--)
+        {
+            std::cout << arr1[i][j];
+            if (j != 0)
+            {
+                std::cout << ", ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    if (fout.is_open())
+    {
+        fout << numb_address << std::endl;
+        for (int i = numb_address - 1; 0 <= i; i--)
+        {
+            for (int j = numb_elements - 1; 0 <= j; j--)
+            {
+                fout << arr1[i][j];
+                if (j != 0)
+                {
+                    fout << ", ";
+                }
+            }
+            fout << std::endl;
+        }
+    }
+    for (int j = 0; j < numb_elements; j++)
+    {
+        delete[] arr1[j];
+    }
+    delete[] arr1;
+}
+fout.close();
+fin.close();
+}
+
